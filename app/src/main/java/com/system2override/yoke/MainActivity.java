8f41a5a2ef5935e2eaea6b4de2958cb9a17745e5
jian <janexhuang@gmail.com>
2018-05-13 14:44:14 -0400

    first commit, also the home button killing thing wooooorks

---
+++ app/src/main/java/com/system2override/yoke/MainActivity.java
@@ -0,0 +1,69 @@
+package com.system2override.yoke;
+
+import android.app.AppOpsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.PACKAGE_USAGE_STATS;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+import static android.support.v4.app.AppOpsManagerCompat.MODE_ALLOWED;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkForUsageStatsPermission(this);
+        checkForAccessFineLocation(this);
+
+        Intent intent = new Intent(this, ManagerService.class);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            android.util.Log.d(TAG, "subscribeToSensor: about to start foreground service " + Long.toString(System.currentTimeMillis()));
+            startForegroundService(intent);
+        } else {
+            android.util.Log.d(TAG, "subscribeToSensor: about to start service " + Long.toString(System.currentTimeMillis()));
+            startService(intent);
+        }
+    }
+
+    private void checkForAccessFineLocation(Context context) {
+        int hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION);
+
+        if (hasAccessFineLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onCreate: about to ask permission");
+            ActivityCompat.requestPermissions(this, new String[] {ACCESS_FINE_LOCATION}, 1);
+        }
+    }
+
+    // this only works with api 22+
+    private void checkForUsageStatsPermission(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            ApplicationInfo appInfo = packageManager.getApplicationInfo("com.system2override.yoke", 0);
+            AppOpsManager appOps = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+            int mode = appOps.checkOpNoThrow(OPSTR_GET_USAGE_STATS, appInfo.uid, context.getPackageName());
+            if (mode != MODE_ALLOWED) {
+                startActivity(new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS));
+            }
+        }  catch (PackageManager.NameNotFoundException e ) {
+            Log.d(TAG, "checkForUsageStatsPermission: oops wrong package name");
+        }
+    }
+}
---
+++ app/src/main/java/com/system2override/yoke/ManagerService.java
@@ -0,0 +1,217 @@
+package com.system2override.yoke;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+
+public class ManagerService extends Service {
+    private static final String TAG = "ManagerService";
+    private List<UsageStats> usageStats;
+    private final int MANAGER_SERVICE_ID = 1;
+
+    public ManagerService() {
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            android.util.Log.d(TAG, "onCreate: about to make this notification");
+            //           /*
+            String CHANNEL_ID = "StepSensorService";
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID,
+                    "StepSensorServiceNotificationChannel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+
+            ((NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE)).createNotificationChannel(channel);
+
+
+            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.placeholder)
+                    .setContentTitle("StepSensor Service in the house")
+                    .setTicker("StepSensorService ticker")
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                    .setWhen(System.currentTimeMillis())
+                    .setContentText("here i yam").build();
+
+
+            startForeground(MANAGER_SERVICE_ID, notification);
+//            */
+        }
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // update with startForegroundService for Oreo services
+
+            try {
+                GetManagerThread getter = new GetManagerThread();
+                getter.run();
+                getter.join();
+                usageStats = getter.getStats();
+
+            for (int i = 0; i < usageStats.size(); i++) {
+                UsageStats stat = usageStats.get(i);
+                Log.d(TAG, "onStartCommand: a usage stat " + stat.getPackageName());
+                Log.d(TAG, "onStartCommand: a usage stat " + Long.toString(stat.getTotalTimeInForeground()));
+            }
+
+
+            } catch (Exception e) {
+                Log.d(TAG, "onStartCommand: stats not gottten");
+            }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        return super.onUnbind(intent);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        super.onRebind(intent);
+    }
+
+    @Override
+    public void onTaskRemoved(Intent rootIntent) {
+        super.onTaskRemoved(rootIntent);
+    }
+
+    @Override
+    protected void dump(FileDescriptor fd, PrintWriter writer, String[] args) {
+        super.dump(fd, writer, args);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public UsageStatsManager getUsageStatsManager() {
+
+        if (android.os.Build.VERSION.SDK_INT == 21) {
+            return (UsageStatsManager) this.getSystemService("usagestats");
+
+        } else {
+            return (UsageStatsManager) this.getSystemService(Context.USAGE_STATS_SERVICE);
+        }
+
+    }
+
+    class GetManagerThread extends Thread {
+        private volatile List<UsageStats> stats;
+
+        private Calendar getStart() {
+
+            Calendar date = new GregorianCalendar();
+// reset hour, minutes, seconds and millis
+            date.set(Calendar.HOUR_OF_DAY, 0);
+            date.set(Calendar.MINUTE, 0);
+            date.set(Calendar.SECOND, 0);
+            date.set(Calendar.MILLISECOND, 0);
+            Log.d(TAG, "getStart: date start " + date.toString());
+            return date;
+        }
+
+        private Calendar getEnd() {
+            Calendar date = new GregorianCalendar();
+// reset hour, minutes, seconds and millis
+            date.set(Calendar.HOUR_OF_DAY, 0);
+            date.set(Calendar.MINUTE, 0);
+            date.set(Calendar.SECOND, 0);
+            date.set(Calendar.MILLISECOND, 0);
+            date.add(Calendar.DAY_OF_MONTH, 1);
+            Log.d(TAG, "getStart: date start " + date.toString());
+            return date;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        public void run()  {
+            UsageStatsManager manager = getUsageStatsManager();
+            Log.d(TAG, "run: foo");
+            Calendar start = getStart();
+            Calendar end = getEnd();
+            stats = manager.queryUsageStats(UsageStatsManager.INTERVAL_DAILY, start.getTimeInMillis(), end.getTimeInMillis());
+            while(true) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) { }
+                Log.d(TAG, "run: " + stats.toString());
+                /*
+                Intent startMain = new Intent(Intent.ACTION_MAIN);
+                startMain.addCategory(Intent.CATEGORY_HOME);
+                startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(startMain);
+                */
+
+            }
+        }
+
+        public List<UsageStats> getStats() {
+            return stats;
+        }
+    }
+    class Killmonger extends Thread {
+
+        @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+        public void run() {
+            while(true) {
+
+            }
+        }
+
+    }
+}
